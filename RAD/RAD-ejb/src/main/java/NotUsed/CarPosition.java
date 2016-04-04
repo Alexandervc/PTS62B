c437429c47a21395d6fde600cf24c9174a685779
lotje1990 <lotje1990@hotmail.com>
2016-04-04 09:00:12 +0200

    change test

--- RAD/RAD-ejb/src/main/java/NotUsed/CarPosition.java
+++ RAD/RAD-ejb/src/main/java/NotUsed/CarPosition.java
@@ -28,6 +28,6 @@ public class CarPosition {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.road = road;
-        this.km = km;
+        //this.km = km;
     }
 }
