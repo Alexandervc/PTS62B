de37ee7eb6eb73e0614063bcdf28a7186dac4062
xChocoMel <melaniekwetters@hotmail.com>
2016-04-05 16:40:16 +0200

    clean

--- Monitoring_System/Monitoring_System-web/src/main/webapp/resources/js/monitor.js
+++ Monitoring_System/Monitoring_System-web/src/main/webapp/resources/js/monitor.js
@@ -1,22 +1,3 @@
-//google.charts.load("current", {packages:['corechart', 'controls', 'timeline']});
-//google.charts.setOnLoadCallback(drawChart);
-//
-//function drawChart() {
-//    var json = document.getElementById("hiddenField");
-//    var container = document.getElementById('timeline1');
-//    var chart = new google.visualization.Timeline(container);
-//    console.log(json.value);
-//
-//    var obj = JSON.parse(json.value);
-//    
-//    var dataTable = new google.visualization.DataTable();
-//    dataTable.addColumn({type: 'string', id: 'State'});
-//    dataTable.addColumn({type: 'string', id: 'Name'});
-//    dataTable.addColumn({type: 'date', id: 'Start'});
-//    dataTable.addColumn({type: 'date', id: 'End'});
-//    //
-
-
 google.charts.load("current", {packages: ['corechart', 'controls', 'timeline']});
 google.charts.setOnLoadCallback(drawChart);
 
