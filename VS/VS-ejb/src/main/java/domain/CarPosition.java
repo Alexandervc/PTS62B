a84efcfd662d14773d7df2b86ba9ab851a7f5ce7
Alexandervc <a.vancuijk@student.fontys.nl>
2016-04-15 22:06:38 +0200

    VS sonar fix

--- VS/VS-ejb/src/main/java/domain/CarPosition.java
+++ VS/VS-ejb/src/main/java/domain/CarPosition.java
@@ -14,8 +14,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  * A position of the cartracker.
