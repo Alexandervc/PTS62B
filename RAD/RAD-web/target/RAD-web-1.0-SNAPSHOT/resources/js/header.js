20d5798f8a81e11efd46c7e8ae23af5a45362e57
Alexandervc <a.vancuijk@student.fontys.nl>
2016-04-21 14:21:28 +0200

    remove RAD
    remove RAD

--- RAD/RAD-web/target/RAD-web-1.0-SNAPSHOT/resources/js/header.js
+++
@@ -1,9 +0,0 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 120) {
-        $('header').addClass('fixed');
-        $('#page').addClass('top');
-    } else {
-        $('header').removeClass('fixed');
-        $('#page').removeClass('top');
-    }
-});
