f1b4bbfd4bf2cdee0e0f7e44dcab1fa53049866c
Alexandervc <a.vancuijk@student.fontys.nl>
2016-04-21 14:27:37 +0200

    RAD add

---
+++ RAD/RAD-ejb/src/test/java/TestDAOBill.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import business.BillManager;
+import service.RoadUsage;
+import domain.Bill;
+import domain.Person;
+import domain.RoadType;
+import java.util.ArrayList;
+import java.util.List;
+import service.RadService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ *
+ * @author Linda
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestDAOBill {
+    RadService service;
+        
+    @Mock
+    BillManager billManager;
+    
+    Person person1;
+    Bill bill1;
+    
+    public TestDAOBill() {       
+    }
+    
+    @Before
+    public void setUp() {
+        service = new RadService();
+        service.setBillManager(billManager);
+                
+        person1 = new Person("Linda", "van Engelen", "LMJC", "Sibeliuslaan", "83",
+                "5654CV", "Eindhoven", "Nederland");
+        
+        List<RoadUsage> roadUsages = new ArrayList<RoadUsage>();
+        roadUsages.add(new RoadUsage("Rachelsmolen", RoadType.C, 5.00));
+        
+        bill1 = new Bill(person1, roadUsages, 10.35, 5L, "april", "2016");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testAddBill(){
+       service.addBill(bill1);
+        verify(billManager, Mockito.times(1)).createBill(bill1);
+    }
+}
---
+++ RAD/RAD-ejb/src/test/java/TestDAOPerson.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import business.PersonManager;
+import domain.Person;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+import service.RadService;
+
+/**
+ *
+ * @author Linda
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestDAOPerson {
+    RadService service;
+    
+    @Mock
+    PersonManager personManager;
+    
+    Person person1;
+    
+    public TestDAOPerson() {
+    }
+    
+    @Before
+    public void setUp() {
+        service = new RadService();
+        service.setPersonManager(personManager);
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testAddPerson(){
+        String firstname= "Linda";
+        String lastname ="van Engelen";
+        String initials = "LMJC";
+  
+        String streetname = "Sibeliuslaan";
+        String number = "83";
+        String zipcode = "5654CV";
+        String city = "Eindhoven";
+        String country = "Nederland";
+        service.addPerson(firstname, lastname, initials, streetname, number, zipcode, city, country);
+        verify(personManager, Mockito.times(1)).createPerson(firstname, lastname, initials, streetname, number, zipcode, city, country);
+    }
+}
