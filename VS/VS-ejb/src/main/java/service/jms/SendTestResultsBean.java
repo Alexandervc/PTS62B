e152013a7d6bdd142ee46d3351f7d29b34dd2910
Alexandervc <a.vancuijk@student.fontys.nl>
2016-05-11 20:02:11 +0200

    Revert "+ VS monitoring JMS transaction"
    This reverts commit 0bda04cb77b8c6ae9446205cf9b31f4d660cab55.

--- VS/VS-ejb/src/main/java/service/jms/SendTestResultsBean.java
+++ VS/VS-ejb/src/main/java/service/jms/SendTestResultsBean.java
@@ -12,8 +12,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.jms.Destination;
 import javax.jms.JMSConnectionFactory;
@@ -27,7 +25,6 @@ import javax.jms.MapMessage;
  * @author Edwin.
  */
 @Stateless
-@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 public class SendTestResultsBean {
     
     private static final Logger LOGGER = Logger
