afe8b81640058e866309e8533f718b19c7260531
lotje1990 <lotje1990@hotmail.com>
2016-05-31 13:55:33 +0200

    PersonDTO

---
+++ RAD/RAD-ejb/src/main/java/dto/PersonDto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import domain.Address;
+
+/**
+ *
+ * @author Linda
+ */
+public class PersonDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String initials;
+    private Address address;
+    //private List<CarDto> cars;
+
+    public PersonDto(Long id, String firstName, String lastName, String initials, Address address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.initials = initials;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    
+}
