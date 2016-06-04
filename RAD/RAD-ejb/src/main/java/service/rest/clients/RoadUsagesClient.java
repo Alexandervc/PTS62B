b11c9be7fbb63e9ed6080064ced58e889ca571ee
Alexandervc <a.vancuijk@student.fontys.nl>
2016-06-04 16:46:49 +0200

    * production settings

--- RAD/RAD-ejb/src/main/java/service/rest/clients/RoadUsagesClient.java
+++ RAD/RAD-ejb/src/main/java/service/rest/clients/RoadUsagesClient.java
@@ -24,11 +24,11 @@ import javax.ws.rs.core.Response;
 public class RoadUsagesClient {
 
     private static final String BASE_URL
-            = "http://localhost:8080/VS-web/vsapi";
+            = "http://192.168.24.72:8080/VS-web/vsapi";
 
     // TODO DEPLOY: UNCOMMENT
     //private static final String BASE_URL =
-    //        "http://localhost:8080/VS-web/vsapi";
+    //        "http://192.168.24.72:8080/VS-web/vsapi";
     
     private Client client;
 
