b705d8371432453a682db37235c24f1c96e30897
Alexandervc <a.vancuijk@student.fontys.nl>
2016-06-06 16:27:13 +0200

    * to production

--- RAPP/src/main/java/service/rest/clients/BillClient.java
+++ RAPP/src/main/java/service/rest/clients/BillClient.java
@@ -34,11 +34,11 @@ public class BillClient {
     private static final Logger LOG = Logger.
             getLogger(BillClient.class.getName());
     private static final String BASE_URL
-            = "http://localhost:8080/RAD-web/radapi";
+            = "http://192.168.24.74:8080/RAD/radapi";
 
     // TODO DEPLOY: UNCOMMENT
     //private static final String BASE_URL = 
-    //        "http://localhost:8080/RAD-web/radapi";
+    //        "http://192.168.24.74:8080/RAD/radapi";
     private Client client;
 
     @PostConstruct
