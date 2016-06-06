a17e33927387eda8bd2c823abb08258eced10a8d
Jesse <j.vanbloemenhuis@student.fontys.nl>
2016-06-06 11:07:32 +0200

    * Uncommented addressList

--- VS/VS-ejb/src/main/java/service/jms/ReceiveForeignCountryMessagesBean.java
+++ VS/VS-ejb/src/main/java/service/jms/ReceiveForeignCountryMessagesBean.java
@@ -39,10 +39,8 @@ import javax.jms.TextMessage;
  */
 @MessageDriven(mappedName = "jms/CS/filteredQueue", activationConfig = {
     @ActivationConfigProperty(propertyName = "messageSelector",
-            propertyValue = "countryCodeTo='PT'"),
-// TODO DEPLOY: UNCOMMENT
-    @ActivationConfigProperty(propertyName = "addressList",
-            propertyValue = "192.168.24.68:7676")
+            propertyValue = "countryCodeTo='PT'")
+    //,@ActivationConfigProperty(propertyName = "addressList", propertyValue = "192.168.24.68:7676")
 })
 public class ReceiveForeignCountryMessagesBean implements MessageListener {
     
