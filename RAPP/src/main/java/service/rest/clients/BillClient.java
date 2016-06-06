3bd8a349bdc4d0ef30b209dc6e8b99446ebcdcb0
Alexandervc <a.vancuijk@student.fontys.nl>
2016-06-06 16:25:11 +0200

    * to development

--- RAPP/src/main/java/service/rest/clients/BillClient.java
+++ RAPP/src/main/java/service/rest/clients/BillClient.java
@@ -38,7 +38,7 @@ public class BillClient {
 
     // TODO DEPLOY: UNCOMMENT
     //private static final String BASE_URL = 
-    //        "http://192.168.24.74:8080/RAD/radapi";
+    //        "http://localhost:8080/RAD-web/radapi";
     private Client client;
 
     @PostConstruct
