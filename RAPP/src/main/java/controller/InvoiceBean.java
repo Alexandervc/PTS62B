f0aeab905551a2a00180af8b9d00ec1b2d3200e1
xChocoMel <melaniekwetters@hotmail.com>
2016-06-14 12:41:50 +0200

    Rapp pdf done

--- RAPP/src/main/java/controller/InvoiceBean.java
+++ RAPP/src/main/java/controller/InvoiceBean.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import dto.BillDto;
-import dto.ListBoxDate;
 import domain.RoadType;
 import dto.BillRoadUsage;
 import dto.CarDto;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormatSymbols;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -17,9 +18,20 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.primefaces.context.RequestContext;
 import service.CarPositionService;
 import service.rest.clients.BillClient;
@@ -31,7 +43,6 @@ import service.rest.clients.BillClient;
 @Named
 @RequestScoped
 public class InvoiceBean {
-
     @Inject
     private InvoiceSession session;
 
@@ -43,13 +54,6 @@ public class InvoiceBean {
 
     private List<BillDto> bills;
     private List<CarDto> cars;
-    //Current month and year.
-    private int year;
-    private int month;
-
-    //Dates for combobox.
-    private String dateIndex;
-    private List<ListBoxDate> dates;
     
     //Locale Nederland
     private Locale locale;
@@ -62,29 +66,6 @@ public class InvoiceBean {
         //Set locale
         this.locale = new Locale("nl", "NL");
 
-        //Setup dates.
-        //Current date.
-        GregorianCalendar cal = new GregorianCalendar();
-        this.year = cal.get(GregorianCalendar.YEAR);
-        this.month = cal.get(GregorianCalendar.MONTH) + 1;
-        this.dateIndex = "0";
-
-        //Create list with ListBoxDate's.
-        this.dates = new ArrayList<>();
-
-        for (int m = 0; m < 25; m++) {
-            GregorianCalendar m_cal = new GregorianCalendar();
-            m_cal.add(Calendar.MONTH, -m);
-            int m_year = m_cal.get(Calendar.YEAR);
-            String m_month_string = m_cal.getDisplayName(
-                    Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
-
-            //Add date to list.
-            String index = Integer.toString(m);
-            String value = m_month_string + " " + m_year;
-            this.dates.add(new ListBoxDate(value, index));
-        }
-
         //Generate carsfor person.
         this.generateCars();
         this.bills = new ArrayList<>();
@@ -92,7 +73,8 @@ public class InvoiceBean {
         
         //Setup maps
         RequestContext requestContext = RequestContext.getCurrentInstance();  
-        requestContext.execute("setDate(" + month + ", " + year + ")");
+        requestContext.execute("setDate(" + this.session.getMonth() + ", " + 
+                this.session.getYear() + ")");
     }
 
     /**
@@ -102,7 +84,7 @@ public class InvoiceBean {
         //Get all bills.
         for (CarDto car : this.cars) {
             this.bills.add(this.client.getBill(car.getCartrackerId(), 
-                    this.month, this.year));
+                    this.session.getMonth(), this.session.getYear()));
         }
     }
 
@@ -112,6 +94,160 @@ public class InvoiceBean {
     public void generateCars() {
         //Get all bills.
         this.cars = this.client.getCars(this.session.restLinkCars());
+    }
+    
+    /**
+     * Generate pdf for given bill.
+     * @param bill choosen bill.
+     */
+    public void generatePdf(BillDto bill) {
+        String name = this.session.getPersonName();
+        String address = this.session.getPersonAddress();
+        String month_string = new DateFormatSymbols(Locale.ENGLISH)
+                .getMonths()[this.session.getMonth()-1];
+        
+        String fileName = "invoice_" + this.session.getYear() + "-" + 
+                this.session.getMonth()+ "_" + bill.getCartrackerId() + ".pdf";         
+        File outputPath = new File("C:\\Proftaak\\invoices\\" + fileName);
+        
+        System.out.println("Generating pdf: " + fileName);
+        
+        try {
+            //Create pdf
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage();
+
+            //Adding page to document
+            document.addPage(page);
+
+            //Adding font to document
+            PDFont font = PDType1Font.HELVETICA;
+            PDFont fontBold = PDType1Font.HELVETICA_BOLD;
+        
+            //Retrieve image to be added to the PDF             
+            File brand = new File("C:\\Proftaak\\rekeningrijden.png");
+            PDImageXObject imageObject = PDImageXObject
+                    .createFromFileByContent(brand, document);
+
+            //Define the content stream
+            //A4 size pixels 75 dpi: 620 x 877
+            PDPageContentStream contentStream = 
+                    new PDPageContentStream(document, page);
+            
+            //Draw brand image
+            contentStream.drawImage(imageObject, 238, 740, 144, 38);
+            
+            //Keep track of yposition
+            Float ypos = 700F;
+            
+            //Info
+            this.writeText(contentStream, font, 11, 75F, ypos, "Name:");
+            this.writeText(contentStream, font, 11, 175F, ypos, name);
+            ypos -= 15;
+            this.writeText(contentStream, font, 11, 75F, ypos, "Address:");
+            this.writeText(contentStream, font, 11, 175F, ypos, address);
+            ypos -= 15;
+            this.writeText(contentStream, font, 11, 75F, ypos, "Cartracker:");
+            this.writeText(contentStream, font, 11, 175F, ypos, 
+                    bill.getCartrackerId());            
+            ypos -= 15;
+            this.writeText(contentStream, font, 11, 75F, ypos, "Fuel:");
+            this.writeText(contentStream, font, 11, 175F, ypos, 
+                    this.getCar(bill.getCartrackerId()).getFuel().toString());
+            ypos -= 15;
+            this.writeText(contentStream, font, 11, 75F, ypos, "Date:");            
+            this.writeText(contentStream, font, 11, 175F, ypos, 
+                    month_string + " " + this.session.getYear());
+            ypos -= 30;
+            
+            //Invoice title
+            this.writeText(contentStream, font, 16, 75F, ypos, "Invoice");
+            ypos -= 25;
+            
+            //Invoice
+            this.writeText(contentStream, fontBold, 11, 75F, ypos, "RoadType");
+            this.writeText(contentStream, fontBold, 11, 200F, ypos, "RoadName");
+            this.writeText(contentStream, fontBold, 11, 325F, ypos, "Km");
+            this.writeText(contentStream, fontBold, 11, 400F, ypos, "Price/km");
+            this.writeText(contentStream, fontBold, 11, 475F, ypos, "Price");
+            ypos -= 15;
+            
+            if (bill.getRoadUsages().size() > 0) {                
+                for (BillRoadUsage ru : bill.getRoadUsages()) {
+                    Float ypos_line = ypos + 11;
+                    contentStream.drawLine(75, ypos_line, 545, ypos_line);
+                    this.writeText(contentStream, font, 11, 75F, ypos, this.getRoadType(ru));
+                    this.writeText(contentStream, font, 11, 200F, ypos, ru.getRoadName());
+                    this.writeText(contentStream, font, 11, 325F, ypos, this.getKm(ru));
+                    this.writeText(contentStream, font, 11, 400F, ypos, this.getRate(ru));
+                    this.writeText(contentStream, font, 11, 475F, ypos, this.getPrice(ru));
+                    ypos -= 15;
+                }
+            }
+            
+            Float ypos_line = ypos + 11;
+            contentStream.drawLine(75, ypos_line, 545, ypos_line);
+            this.writeText(contentStream, fontBold, 11, 75F, ypos, "Total:");  
+            this.writeText(contentStream, font, 11, 475F, ypos, this.getTotalPrice(bill));
+            
+            //Close content stream
+            contentStream.close();
+            
+            //Save the PDF
+            OutputStream output = new FileOutputStream(outputPath); 
+            document.save(output);
+            document.close();
+            output.close(); 
+            
+            InputStream input = new FileInputStream(outputPath);
+            this.download(IOUtils.toByteArray(input), "application/pdf", fileName);
+            
+            System.out.println("Pdf done");
+        } catch (IOException ex) {
+            Logger.getLogger(InvoiceBean.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+
+    /**
+     * Write a line in the given pdf file.
+     * @param contentStream pdf file.
+     * @param font font style.
+     * @param fontsize font size.
+     * @param x pixels of page.
+     * @param y pixels of page.
+     * @param text text to write.
+     * @throws IOException if process fails.
+     */
+    private void writeText(PDPageContentStream contentStream, PDFont font, 
+            Integer fontsize, Float x, Float y, String text) 
+            throws IOException {
+        contentStream.beginText();
+        contentStream.setFont(font, fontsize);
+        contentStream.newLineAtOffset(x, y);
+        contentStream.showText(text);
+        contentStream.endText();
+    }
+
+    /**
+     * Put file in downloads folder by sending response.
+     * @param exportContent file to byte array.
+     * @param contentType type of content.
+     * @param fileName name of file.
+     * @throws IOException if process fails.
+     */
+    public void download(byte[] exportContent, String contentType, String fileName) throws IOException {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+
+        ec.responseReset(); 
+        ec.setResponseContentType(contentType);
+        ec.setResponseContentLength(exportContent.length);
+        ec.setResponseHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
+        OutputStream output = ec.getResponseOutputStream();        
+        output.write(exportContent);
+
+        fc.responseComplete();
     }
     
     public List<BillDto> getBillFromCar(String cartracker){
@@ -129,19 +265,20 @@ public class InvoiceBean {
      * Listener for date dropdown menu.
      */
     public void changeDate() {
-        int index = Integer.parseInt(this.dateIndex);
+        int index = Integer.parseInt(this.session.getDateIndex());
 
         GregorianCalendar cal = new GregorianCalendar();
         cal.add(Calendar.MONTH, -index);
-        this.year = cal.get(Calendar.YEAR);
-        this.month = cal.get(Calendar.MONTH) + 1;
+        this.session.setYear(cal.get(Calendar.YEAR));
+        this.session.setMonth(cal.get(Calendar.MONTH) + 1);
         
         //Get all bills.
         this.generateBills();
         
         //Setup maps
         RequestContext requestContext = RequestContext.getCurrentInstance();  
-        requestContext.execute("setDate(" + month + ", " + year + ")");
+        requestContext.execute("setDate(" + this.session.getMonth() + ", " + 
+                this.session.getYear() + ")");
         requestContext.execute("setupEvents()");
     }
 
@@ -218,21 +355,19 @@ public class InvoiceBean {
     public List<CarDto> getCars() {
         return new ArrayList<>(this.cars);
     }
-
-    public String getDateIndex() {
-        return this.dateIndex;
-    }
-
-    public void setDateIndex(String dateIndex) {
-        this.dateIndex = dateIndex;
-    }
-
-    public List<ListBoxDate> getDates() {
-        return new ArrayList<>(this.dates);
+    
+    public CarDto getCar(String cartrackerId) {
+        for (CarDto car : this.getCars()) {
+            if (car.getCartrackerId().equals(cartrackerId)) {
+                return car;
+            }
+        }
+        
+        return null;
     }
 
     public String getCoordinates(String cartrackerId) {
-        return this.positionService
-                .getCoordinates(cartrackerId, this.month, this.year);
+        return this.positionService.getCoordinates(cartrackerId, 
+                this.session.getMonth(), this.session.getYear());
     }
 }
--- RAPP/src/main/java/controller/InvoiceSession.java
+++ RAPP/src/main/java/controller/InvoiceSession.java
@@ -1,12 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import dto.ListBoxDate;
 import dto.PersonDto;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -19,6 +21,40 @@ import javax.inject.Named;
 public class InvoiceSession implements Serializable {
     private Long personId;
     private PersonDto person;
+    
+    //Current month and year.
+    private int year;
+    private int month;
+
+    //Dates for combobox.
+    private String dateIndex;
+    private List<ListBoxDate> dates;
+    
+    @PostConstruct
+    public void setup() {
+                //Setup dates.
+        //Current date.
+        GregorianCalendar cal = new GregorianCalendar();
+        this.year = cal.get(GregorianCalendar.YEAR);
+        this.month = cal.get(GregorianCalendar.MONTH) + 1;
+        this.dateIndex = "0";
+
+        //Create list with ListBoxDate's.
+        this.dates = new ArrayList<>();
+
+        for (int m = 0; m < 25; m++) {
+            GregorianCalendar m_cal = new GregorianCalendar();
+            m_cal.add(Calendar.MONTH, -m);
+            int m_year = m_cal.get(Calendar.YEAR);
+            String m_month_string = m_cal.getDisplayName(
+                    Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+
+            //Add date to list.
+            String index = Integer.toString(m);
+            String value = m_month_string + " " + m_year;            
+            this.dates.add(new ListBoxDate(value, index));            
+        } 
+    }
 
     public Long getPersonId() {
         return personId;
@@ -53,5 +89,33 @@ public class InvoiceSession implements Serializable {
                 this.person.getAddress().getHousenumber() +
                 ", " + this.person.getAddress().getZipcode() +" "+
                 this.person.getAddress().getCity();
+    }
+    
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    
+    public String getDateIndex() {
+        return this.dateIndex;
+    }
+
+    public void setDateIndex(String dateIndex) {
+        this.dateIndex = dateIndex;
+    }
+
+    public List<ListBoxDate> getDates() {
+        return new ArrayList<>(this.dates);
     }
 }
