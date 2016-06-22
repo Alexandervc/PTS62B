b97740ddd037c30973adc6d21853c1f7cb75263d
Alexandervc <a.vancuijk@student.fontys.nl>
2016-06-22 19:46:59 +0200

    * check roadUsage foreigncountryrideid null

--- VS/VS-ejb/src/main/java/dao/RoadUsageDao.java
+++ VS/VS-ejb/src/main/java/dao/RoadUsageDao.java
@@ -14,19 +14,14 @@ import domain.CarPosition;
 import domain.RoadType;
 import dto.RoadUsage;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * The dao for road usages.
+ *
  * @author Edwin
  */
 @Stateless
 public class RoadUsageDao extends AbstractDaoFacade<CarPosition> {
-    
-    private static final Logger LOGGER
-            = Logger.getLogger(RoadUsageDao.class.getName());
-    
     @PersistenceContext
     private EntityManager em;
 
@@ -41,18 +36,19 @@ public class RoadUsageDao extends AbstractDaoFacade<CarPosition> {
     protected EntityManager getEntityManager() {
         return this.em;
     }
-    
+
     /**
      * Gets RoadUsages from the materializedView based on a cartrackerid, a
- month and a year.
-     * @param cartrackerid The id of the cartracker with the roadusages that 
-     *      are requested.
+     * month and a year.
+     *
+     * @param cartrackerid The id of the cartracker with the roadusages that are
+     * requested.
      * @param month The month that is being requested.
      * @param year The year the month is in.
      * @return A list of RoadUsages from the person.
      */
     public List<RoadUsage> getRoadUsages(String cartrackerid,
-            String month, String year ) {
+            String month, String year) {
         Query query = this.em.createNativeQuery(
                 "select * from MV_roadusage"
                 + " WHERE cartracker_id = ? AND month = ? AND year = ?");
@@ -61,22 +57,16 @@ public class RoadUsageDao extends AbstractDaoFacade<CarPosition> {
         query.setParameter(3, year);
 
         List<RoadUsage> returnList = new ArrayList<>();
-        List<Object[]> roadUsages = query.getResultList(); 
-        for(Object[] o : roadUsages) {
+        List<Object[]> roadUsages = query.getResultList();
+        for (Object[] o : roadUsages) {
             String roadName = o[3].toString();
             Double km = Double.valueOf(o[5].toString());
             RoadType type = RoadType.valueOf(o[4].toString());
-            Long foreignCountryRoadId;
-            try {
+            Long foreignCountryRoadId = null;
+            if (o[6] != null) {
                 foreignCountryRoadId = Long.valueOf(o[6].toString());
-            } catch (NumberFormatException ex) {
-                foreignCountryRoadId = null;
-                LOGGER.log(Level.INFO,null,ex);
             }
-            RoadUsage usage = new RoadUsage(roadName
-                    ,type
-                    ,km
-                    ,foreignCountryRoadId);
+            RoadUsage usage = new RoadUsage(roadName, type, km, foreignCountryRoadId);
             returnList.add(usage);
         }
         return returnList;
